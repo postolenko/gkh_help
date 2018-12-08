d53afeb65c59a5b078d44a3ab16805aa530c75dd
postolenko <denis.postolenko@gmail.com>
2018-12-08 18:18:24 +0200

    GKH

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,52 @@ $(window).resize(function() {
 
 $(document).ready(function() {   
 
+	$(".show_popup").click(function(e) {
+
+        e.preventDefault();
+
+        popupName = $(this).attr("data-popup-name");
+        popupBlock = $("[data-popup = '"+ popupName +"']");
+
+        popupBlock.fadeIn(400);
+
+    });
+
+     $(this).keydown(function(eventObject){
+
+        if (eventObject.which == 27) {
+
+            if ( $(".popup_wrapp").is(":visible") ) {
+
+                $(".popup_wrapp").fadeOut(300);
+
+            }
+
+        }
+
+    });
+
+    $(".close-popup").click(function() {
+
+        popupBlock = $(this).closest(".popup_wrapp");
+
+        popupBlock.fadeOut(300);
+
+    });
+
+
+    $(document).mouseup(function (e){
+
+        hide_element = $('.popup');
+
+        if (!hide_element.is(e.target)
+
+            && hide_element.has(e.target).length === 0) {
+
+            hide_element.closest(".popup_wrapp").fadeOut(300);
+        }
+
+    });
 
 });
 
